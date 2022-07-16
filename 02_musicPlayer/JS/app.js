d9c37f5c18f92bbc6888d3147d2a6fbc2747d0cc
jackerbell <jackerbell@naver.com>
2022-07-16 22:10:57 +0900

    2022-07-16 update
    세부사항은 각 프로젝트 폴더의 README.md 파일에 기입
    기존 통합 버전에서 개별적으로 분리
    내일 중으로 musicPlayer 음원재생 및 재생옵션 기능 추가 예정..
    작업완료는 아마 2~3일 정도 더 요구될 것으로 예상..

--- 02_musicPlayer/JS/app.js
+++ 02_musicPlayer/JS/app.js
@@ -1,14 +1,111 @@
-function changeBox() {
-    const first = document.getElementById('page1')
-    const second = document.getElementById('page2')
-    const button = document.getElementById('btn');
-    if (button.innerHTML == 'page1') {
-        first.style.display = 'none'
-        second.style.display = 'block'
-        button.innerHTML = 'page2'
-    } else if (button.innerHTML = 'page2') {
-        first.style.display = 'block'
-        second.style.display = 'none'
-        button.innerHTML = 'page1'
+document.addEventListener("DOMContentLoaded", () => {
+  changePg();
+  search();
+  playOption();
+  heart();
+  controller1();
+  controller2();
+});
+
+function changePg() {
+  var first = document.getElementById('page1');
+  var second = document.getElementById('page2');
+  var button1 = document.querySelector('#changebtn1');
+  var button2 = document.querySelector('#changebtn2');
+
+  button1.addEventListener('click',function(){
+    /* addEventListener issue로 인해 script(app.js)위치를 body 최하단으로 이동 */
+      first.style.display = 'none';
+      second.style.display = 'block';
     }
+  )
+  button2.addEventListener('click',function(){
+    /* addEventListener issue로 인해 script(app.js)위치를 body 최하단으로 이동 */
+      first.style.display = 'block';
+      second.style.display = 'none';
+    }
+  )
+}
+
+function search() {
+  var input = document.querySelector('#input');
+  var search = document.querySelector('.search');
+  var header = document.querySelector('#header');
+  var changebtn = document.querySelector('#changebtn1');
+
+  search.addEventListener('click',function(){
+    header.classList.toggle('searchMode')
+    if(input.style.display == 'none'){
+      input.style.display = 'block';
+      changebtn.style.display = 'none';
+    } else{
+      input.style.display = 'none';
+      changebtn.style.display = 'inline-block';
+    }      
+  })
+}
+
+function playOption() {
+  var option = document.querySelectorAll('.option');
+
+  option[0].addEventListener('click',function(){ /*foreach를 쓰지 않을 경우 배열의 순서를 꼭 적어줘야함! */
+    option[0].style['display'] = 'none';
+    option[1].style['display'] = 'inline';
+  })
+  option[1].addEventListener('click',function(){ 
+    option[1].style['display'] = 'none';
+    option[2].style['display'] = 'inline';
+  })
+  option[2].addEventListener('click',function(){ 
+    option[2].style['display'] = 'none';
+    option[0].style['display'] = 'inline';
+  })
+}
+
+function heart() {
+  var heart = document.querySelector('#heart');
+  var heartNum = document.querySelector('#heartNum');
+  var icon = document.querySelector('.fa-heart');
+  heartInt0 = parseInt(heartNum.innerText); // 좋아요 한번만 누르기 위한 조건용.
+  heartInt1 = parseInt(heartNum.innerText);
+  heart.addEventListener('click',function(){
+    if(heartInt0 >= heartInt1){
+      heartInt1 += 1;
+      heartNum.innerText = heartInt1;
+      /*
+      if(heartInt1 >= 1000 && heartInt1 < 1000000){
+        heartNum.innerText = Math.round(heartInt1/1000).toFixed(2); 
+        heartNum.innerText += 'K'
+      } else {
+        heartNum.innerText = Math.round(heartInt1/1000000).toFixed(2); 
+        heartNum.innerText += 'M'
+      }
+      */
+      icon.classList.add('heartEffection');
+    }
+  })
+}
+
+function controller1() {
+  var play1 = document.querySelectorAll('.play1');
+  play1[0].addEventListener('click', function(){
+    play1[0].style['display'] = 'none';
+    play1[1].style['display'] = 'inline-block';
+  })
+  play1[1].addEventListener('click', function(){
+    play1[1].style['display'] = 'none';
+    play1[0].style['display'] = 'inline-block';
+  })
+}
+
+function controller2() {
+  var play2 = document.querySelectorAll('.play2');
+  play2[0].addEventListener('click', function(){
+    play2[0].style['display'] = 'none';
+    play2[1].style['display'] = 'inline-block';
+  })
+  play2[1].addEventListener('click', function(){
+    play2[1].style['display'] = 'none';
+    play2[0].style['display'] = 'inline-block';
+  })
 }
