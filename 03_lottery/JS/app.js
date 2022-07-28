338476055d7d75c7bde7f3ac54952b595b6cadfe
jackerbell <jackerbell@naver.com>
2022-07-29 08:18:56 +0900

    2022-07-29 update
    slot animation 수정 완료

--- 03_lottery/JS/app.js
+++ 03_lottery/JS/app.js
@@ -4,6 +4,13 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 const slot = document.getElementById('slot');
-  slot.addEventListener('click', function() {
-    slot.classList.toggle('slotAni')
-  })
+  slot.addEventListener('click', function () {
+    slot.animate([
+      {transform: 'rotate(0deg)'},
+      {transform: 'rotate(-90deg)'},
+      {transform: 'rotate(0deg)'}
+    ],{
+      duration:1000
+    }) 
+  }
+  )
