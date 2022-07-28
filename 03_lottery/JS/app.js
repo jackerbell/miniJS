b728decec6fc067530e729c20cd1f32401374ffe
jackerbell <jackerbell@naver.com>
2022-07-29 01:13:45 +0900

    2022-07-29 update
    slot 요소에 애니메이션 추가
    현재 클릭이벤트에 토글요소를 넣어서 임시로 두번클릭시 동작하고 있지만 옆에 추첨버튼을 만들어 초기화 버튼으로 설정할 예정

---
+++ 03_lottery/JS/app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+ });
+
+
+const slot = document.getElementById('slot');
+  slot.addEventListener('click', function() {
+    slot.classList.toggle('slotAni')
+  })
