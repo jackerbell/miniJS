d6747407e4f70eca37d1460bbde4dd677be8320d
jackerbell <jackerbell@naver.com>
2022-08-01 21:33:58 +0900

    2022-08-01 update
    lottery 랜덤 뽑기 기능 추가 (미완)
    세부사항은 README.md에 정리

--- 03_lottery/JS/app.js
+++ 03_lottery/JS/app.js
@@ -2,15 +2,74 @@ document.addEventListener("DOMContentLoaded", function(){
 
  });
 
-
+const myNum = document.querySelectorAll(".myNum");
+const winningNum = document.querySelectorAll(".winningNum");
 const slot = document.getElementById('slot');
-  slot.addEventListener('click', function () {
-    slot.animate([
-      {transform: 'rotate(0deg)'},
-      {transform: 'rotate(-90deg)'},
-      {transform: 'rotate(0deg)'}
-    ],{
-      duration:1000
-    }) 
+var i = 0;
+var j = 0;
+var numList = null;
+var numList2 = null;
+function slotWork(){
+  
+}
+slot.addEventListener('click', function () {
+  slot.animate([
+    {transform: 'rotate(0deg)'},
+    {transform: 'rotate(-90deg)'},
+    {transform: 'rotate(0deg)'}
+  ],{
+    duration:400
+  }) 
+  numList = lottoNum()
+  for(i=0;i<numList.length;i++){
+    myNum[i].innerHTML = `${numList[i]}`
   }
-  )
+  console.log(myNum)
+  numList2 = lottoNum2()
+  for(j=0;j<numList2.length;j++){
+    winningNum[j].innerHTML = `${numList2[j]}`
+  }
+}
+)
+
+  function lottoNum() {
+    let lotto = []
+    let n = 0;
+    while(i<myNum.length) {
+      n = Math.floor(Math.random() * 45) + 1;
+      if(! sameNum(n)){
+        lotto.push(n);
+        i++;
+      }
+    }
+    function sameNum(n) {
+      for(i=0;i<lotto.length;i++){
+        if(n === lotto[i]){
+          return true
+        }
+      }
+      return false
+    }
+    return lotto
+  }
+
+  function lottoNum2() {
+    let lotto2 = []
+    let n2 = 0;
+    while(j<winningNum.length) {
+      n2 = Math.floor(Math.random() * 45) + 1;
+      if(! sameNum2(n2)){
+        lotto2.push(n2);
+        j++;
+      }
+    }
+    function sameNum2(n2) {
+      for(j=0;j<lotto2.length;j++){
+        if(n2 === lotto2[j]){
+          return true
+        }
+      }
+      return false
+    }
+    return lotto2
+  }
